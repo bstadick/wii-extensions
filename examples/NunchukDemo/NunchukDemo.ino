e360e1bc7e8b63feea9b901cf24b783e343cdb79
bstadick <2057235+bstadick@users.noreply.github.com>
2020-12-26 22:13:20 -0600

    Update license in each file to MIT.

--- examples/NunchukDemo/NunchukDemo.ino
+++ examples/NunchukDemo/NunchukDemo.ino
@@ -1,3 +1,42 @@
+/**
+* @author Bryan Stadick - stadi012@umn.edu
+* @version 1.0.0
+* @file
+*
+* NunchukDemo.ino - Demo for the Wii Nunchuk extension controller
+* <p>
+* Based off project by:
+* Copyright 2011-2013 Gabriel Bianconi, http://www.gabrielbianconi.com/
+* Project URL : http://www.gabrielbianconi.com/projects/ArduinoNunchuck/
+* Based on the following resources: http://wiibrew.org/wiki/Wiimote/Extension_Controllers
+* <p>
+* Wii is a trademark of <a href="https://www.nintendo.com/">Nintendo</a>. The author of this library does not claim
+* any ownership or association with Nintendo or their affiliates.
+* <p>
+* MIT License
+* <p>
+* Copyright (c) 2020 Bryan Stadick
+* <p>
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* <p>
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* <p>
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* <p>
+*/
+
 #include <Wire.h>
 #include <WiiExt.h>
 
